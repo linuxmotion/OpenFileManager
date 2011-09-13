a54eb00a28a372793c363b0ff3eab47df05a6b77
John Weyrauch <jweyrauch359@gmail.com>
2011-09-12 20:56:53 -0700

    Handle the onClick differently
    Handle the onClick by sending all resources as a single intent broadcast. 
    Remove ContextMenu variable that also removes a lot of unneeded code.

--- src/org/linuxmotion/models/onFileClickListener.java
+++ src/org/linuxmotion/models/onFileClickListener.java
@@ -43,295 +43,39 @@ public class onFileClickListener implements OnClickListener, OnLongClickListener
 	private boolean mIsDirectory;
 	private Context mContext;
 	private File mFile;
-	private ContextMenu mContextMenu = new ContextMenu(){
-
-		@Override
-		public void clearHeader() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public ContextMenu setHeaderIcon(int arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public ContextMenu setHeaderIcon(Drawable arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public ContextMenu setHeaderTitle(int arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public ContextMenu setHeaderTitle(CharSequence arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public ContextMenu setHeaderView(View arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public MenuItem add(CharSequence arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public MenuItem add(int arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public MenuItem add(int arg0, int arg1, int arg2, CharSequence arg3) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public MenuItem add(int arg0, int arg1, int arg2, int arg3) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public int addIntentOptions(int arg0, int arg1, int arg2,
-				ComponentName arg3, Intent[] arg4, Intent arg5, int arg6,
-				MenuItem[] arg7) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public SubMenu addSubMenu(CharSequence arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public SubMenu addSubMenu(int arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public SubMenu addSubMenu(int arg0, int arg1, int arg2,
-				CharSequence arg3) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public SubMenu addSubMenu(int arg0, int arg1, int arg2, int arg3) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public void clear() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void close() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public MenuItem findItem(int arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public MenuItem getItem(int arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public boolean hasVisibleItems() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean isShortcutKey(int arg0, KeyEvent arg1) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean performIdentifierAction(int arg0, int arg1) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean performShortcut(int arg0, KeyEvent arg1, int arg2) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public void removeGroup(int arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void removeItem(int arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void setGroupCheckable(int arg0, boolean arg1, boolean arg2) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void setGroupEnabled(int arg0, boolean arg1) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void setGroupVisible(int arg0, boolean arg1) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void setQwertyMode(boolean arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public int size() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		
-	};
+	
 	
 	public onFileClickListener(Context context, File file){
-		this.mIsDirectory = file.isDirectory();
-		this.mContext = context;
-		this.mFile = file;
-		
-		// Is this file if it is not a directory
-		// a image, a video, or a document
+		mIsDirectory = file.isDirectory();
+		mContext = context;
+		mFile = file;
 		
 	}
 	
 	@Override
 	public void onClick(View v) {
 		
-		//v.setFocusable(true);
-		//v.setFocusableInTouchMode(true);
-		//v.setSelected(true);
-		//v.requestFocus();
-		
-		if(this.mIsDirectory){
+		if(mIsDirectory){
 			
-			Log.d(TAG, "Is a directory");
+			Log.d(TAG, "Sending UI refresh broadcast");
 			
 			Intent updateintent = new Intent(Constants.UPDATE_INTENT);
 			updateintent.putExtra("PATH", mFile.getPath());
 			openFileManagerActivity.resetExitStatus();
-			this.mContext.sendBroadcast(updateintent);
+			mContext.sendBroadcast(updateintent);
 			
 		}else{
 			
-			Log.d(TAG, "The file should be opened here");
-			// Open the file here
-			FileType type = FileUtils.checkFileExtension(mFile);
-		
-			switch(type){
-			
-			case IMAGE: 
-				handleImageIntent();
-				break;
-			case PLAIN_TEXT:
-				handlePlainTextIntent();
-				break;
-			case DOCUMENT:
-				handleDocumentIntent();
-				break;
-			case VIDEO:
-				handleVideoIntent();
-				break;
-			case UNKNOWN:
-				handleUnknownIntent();
-				break;
-			}
-			
-			
+			Log.d(TAG, "Sending media broadcast");
+			Intent video_intent = new Intent(Constants.VIDEO_INTENT);
+			resource_intent.putExtra("RESOURCE", mFile.toString());
+			mContext.sendBroadcast(resource_intent);
+					
 			
 		}
-		// TODO Auto-generated method stub
 
 	}
-	private void handleUnknownIntent() {
-		// TODO Auto-generated method stub
-		Log.d(TAG, "Sending unknown braodcast");
-		Intent video_intent = new Intent(Constants.VIDEO_INTENT);
-		video_intent.putExtra("UNKNOWN", "");
-		this.mContext.sendBroadcast(video_intent);
-	}
-	private void handleVideoIntent() {
-		// TODO Auto-generated method stub
-		Log.d(TAG, "Sending video braodcast");
-		Intent video_intent = new Intent(Constants.VIDEO_INTENT);
-		video_intent.putExtra("VIDEO", this.mFile.toString());
-		this.mContext.sendBroadcast(video_intent);
-	}
-
-	private void handleDocumentIntent() {
-		// TODO Auto-generated method stub
-		Log.d(TAG, "Sending document braodcast");
-		Intent document_intent = new Intent(Constants.DOCUMENT_INTENT);
-		document_intent.putExtra("DOCUMENT", this.mFile.toString());
-		this.mContext.sendBroadcast(document_intent);
-	}
-
-	private void handlePlainTextIntent() {
-		// TODO Auto-generated method stub
-		Log.d(TAG, "Sending document braodcast");
-		Intent plain_text_intent = new Intent(Constants.PLAIN_TEXT_INTENT);
-		plain_text_intent.putExtra("DOCUMENT", this.mFile.toString());
-		this.mContext.sendBroadcast(plain_text_intent);
-		
-	}
-
-	private void handleImageIntent() {
-
-		// in onCreate or any event where your want the user to
-        // select a file
-		Log.d(TAG, "Sending image braodcast");
-		Intent image_intent = new Intent(Constants.IMAGE_INTENT);
-		image_intent.putExtra("IMAGE", this.mFile.toString());
-		this.mContext.sendBroadcast(image_intent);
-
-		
-	}
+	priva
 
 	@Override
 	public boolean onLongClick(View v) {
@@ -339,8 +83,6 @@ public class onFileClickListener implements OnClickListener, OnLongClickListener
 		Log.d(TAG, "Long click");
 		v.setSelected(true);
 		v.requestFocus();
-		//v.createContextMenu(mContextMenu);
-		v.setSelected(false);
 		return false;
 	}
 
